e14a485895a8e2e41d5c899ede6b58ee81034861
Ali Alizadeh <alizadeh.dev@gmail.com>
2019-11-26 12:48:45 -0500

    removed back button #25

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row align="center" justify="center">
         <v-col cols="12" md="7" sm="10">
-          <main-panel panelTitle>
+          <main-panel panelTitle :showBackButton="false">
             <template v-slot>
               <p
                 :class="[
